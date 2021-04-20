da078585fba7d6be32c347e618e0dc47d5434de4
Alex Chubaty <achubaty@for-cast.ca>
2021-04-20 10:14:19 -0600

    initial setup

---
+++ 01-init.R
@@ -0,0 +1,36 @@
+if (file.exists(".Renviron")) readRenviron(".Renviron")
+
+.starttime <- Sys.time()
+
+Require("config")
+
+cacheDir <- config::get("paths")[["cachedir"]]
+cacheFormat <- config::get("cacheformat")
+codeChecks <- config::get("codechecks")
+delayStart <- config::get("delaystart")
+eventCaching <- c(".inputObjects", "init")
+messagingNumCharsModule <- config::get("messagingNumCharsModule")
+newGoogleIDs <- FALSE ## gets rechecked/updated for each script (06, 07x, 08x) based on script 05
+reproducibleAlgorithm <- config::get("reproduciblealgorithm")
+run <- config::get("run")
+scratchDir <- config::get("paths")[["scratchdir"]]
+suitabilityIndex <- config::get("mpbsuitabilityindex")
+useMemoise <- config::get("usememoise")
+usePlot <- config::get("plot")
+userInputPaths <- config::get("inputpaths")
+usePrerun <- config::get("useprerun")
+useRequire <- config::get("userequire")
+
+## default paths
+inputDir <- "inputs"
+moduleDir <- "modules"
+outputDir <- "outputs"
+scratchDir <- config::get("paths")[["scratchdir"]]
+
+## runName
+if (!exists("runName")) {
+  runName <- sprintf("run%02d", run) ## TODO: update for MPB climate scenario
+} else {
+  studyAreaName <- strsplit(runName, "_")[[1]][1]
+  run <- as.numeric(substr(runName, nchar(runName) - 1, nchar(runName)))
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# MPB_SK
+
+Summary of project, with a brief description.
+
+## Study Area
+
+![](images/studyArea.png)
+
+## Modules
+
+### Study Area
+
+- add link(s) to (sub)module repo(s)
+
+## Module Group A
+
+- add link(s) to (sub)module repo(s)
+
+## Module Group B
+
+- add link(s) to (sub)module repo(s)
