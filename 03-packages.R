dcf0b1d50b76e84610fecda722e0af1ba39d65f5
Eliot McIntire <eliotmcintire@gmail.com>
2021-05-18 13:05:24 -0700

    03-packages re-order so SpaDES.install is first

--- 03-packages.R
+++ 03-packages.R
@@ -1,5 +1,6 @@
-Require(c("data.table", "plyr", "pryr", "raster")) ## ensure plyr loaded before dplyr or there will be problems
 Require("PredictiveEcology/SpaDES.install (>= 0.0.4.9000)")
+out <- makeSureAllPackagesInstalled(modulePath = "modules")
+Require(c("data.table", "plyr", "pryr", "raster")) ## ensure plyr loaded before dplyr or there will be problems
 Require("PredictiveEcology/SpaDES.core@rasterToMemoryUpdates (>= 1.0.6.9023)",
         which = c("Suggests", "Imports", "Depends"), upgrade = FALSE) # need Suggests in SpaDES.core
 
@@ -7,4 +8,3 @@ Require("achubaty/amc (>= 0.2.0)", require = FALSE, which = c("Suggests", "Impor
 Require("jimhester/archive", upgrade = FALSE)
 Require("slackr", require = FALSE)
 
-out <- makeSureAllPackagesInstalled(modulePath = "modules")
