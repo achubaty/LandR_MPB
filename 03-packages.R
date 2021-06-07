c940a97112ce1b72b869bd2871529eb22b580fca
Eliot McIntire <eliotmcintire@gmail.com>
2021-06-07 14:41:11 -0700

    bring in binary ubuntu libraries to 03-packages

--- 03-packages.R
+++ 03-packages.R
@@ -1,3 +1,20 @@
+## set CRAN repos; use binary linux packages if on Ubuntu
+local({
+  options(Ncpus = parallel::detectCores() / 2)
+  options("repos" = c(CRAN = "https://cran.rstudio.com"))
+
+  if (Sys.info()["sysname"] == "Linux" && grepl("Ubuntu", utils::osVersion)) {
+    .os.version <- strsplit(system("lsb_release -c", intern = TRUE), ":\t")[[1]][[2]]
+    .user.agent <- paste0(
+      "R/", getRversion(), " R (",
+      paste(getRversion(), R.version["platform"], R.version["arch"], R.version["os"]),
+      ")"
+    )
+    options(repos = c(CRAN = paste0("https://packagemanager.rstudio.com/all/__linux__/",
+                                    .os.version, "/latest")))
+    options(HTTPUserAgent = .user.agent)
+  }
+})
 Require("PredictiveEcology/SpaDES.install (>= 0.0.4.9000)")
 out <- makeSureAllPackagesInstalled(modulePath = "modules")
 Require(c("data.table", "plyr", "pryr", "raster")) ## ensure plyr loaded before dplyr or there will be problems
