b5a60047abeab288e3ada9486ccdf4a69130a3b2
Alex Chubaty <achubaty@for-cast.ca>
2021-06-15 12:47:38 -0600

    install BioSIM before trying to install module reqdPkgs

--- 03-packages.R
+++ 03-packages.R
@@ -7,6 +7,14 @@ if (gdal_version > "3.2" | proj_version > "7.2") {
   install.packages(spatialPkgs, repos = "https://cran.rstudio.com")
 }
 
+if (!require("BioSIM", quietly = TRUE)) {
+  ## https://sourceforge.net/p/mrnfforesttools/biosimclient/wiki/BioSIM-R/#requirements
+  install.packages("https://sourceforge.net/projects/repiceasource/files/latest",
+                   repos = NULL,  type = "source")
+  install.packages("https://sourceforge.net/projects/biosimclient.mrnfforesttools.p/files/latest",
+                   repos = NULL,  type = "source")
+}
+
 Require("PredictiveEcology/SpaDES.install (>= 0.0.4.9000)")
 out <- makeSureAllPackagesInstalled(modulePath = "modules")
 Require(c("data.table", "plyr", "pryr", "raster")) ## ensure plyr loaded before dplyr or there will be problems
@@ -16,11 +24,3 @@ Require("SpaDES.core (>= 1.0.8)",
 Require("achubaty/amc (>= 0.2.0)", require = FALSE, which = c("Suggests", "Imports", "Depends"))
 Require("jimhester/archive", upgrade = FALSE)
 Require("slackr", require = FALSE)
-
-if (!require("BioSIM", quietly = TRUE)) {
-  ## https://sourceforge.net/p/mrnfforesttools/biosimclient/wiki/BioSIM-R/#requirements
-  install.packages("https://sourceforge.net/projects/repiceasource/files/latest",
-                   repos = NULL,  type = "source")
-  install.packages("https://sourceforge.net/projects/biosimclient.mrnfforesttools.p/files/latest",
-                   repos = NULL,  type = "source")
-}
